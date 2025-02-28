eba308454d17629e076a8f0e64c6d95bf661f1bd
Marc Pham <112006435+48774861@users.noreply.github.com>
2025-02-28 01:08:45 +0000

    Update program1.c

--- program1.c
+++ program1.c
@@ -3,36 +3,43 @@
 #include <unistd.h>
 #include <pthread.h>
 
+// Defines a struct, with two integers that will be passed as parameters to the thread's function.
 typedef struct {
-    int m;
-    int n;
+	int m;
+	int n;
 } ThreadParams;
 
-void *thread_function(void *arg);
+void *thread_function(void *arg); // Thread Function Declaration.
 
 int main() {
-	pthread_t a_thread; //thread declaration
-	ThreadParams params;
-	params.m = 5;
-	params.n = 7;
+	pthread_t a_thread; // Thread Declaration
 	
-	void *result;
+	// Define the parameters that will be passed to the function.
+	ThreadParams params;
+	params.m = 9692;
+	params.n = 2813;
+	
+	void *result; // Result of m+n returned by the thread.
+	
+	// Create the thread and wait for the thread to return the result with join.
 	pthread_create(&a_thread, NULL, thread_function, &params);
-	//thread is created
-	pthread_join(a_thread, &result); //process waits for thread to finish .
-	//Comment this line to see the difference
+	pthread_join(a_thread, &result);
+	
+	// Print m + n = result.
+	printf("Outside Thread\n");
 	printf("%d",params.m);
 	printf(" + ");
 	printf("%d",params.n);
 	printf(" = ");
-	printf("%d",(int)result);
+	printf("%d\n",(int)(intptr_t)result); // Cast the result from void* to int for printing.
 }
 void *thread_function(void *arg) {
-	// the work to be done by the thread is defined in this function
+
 	printf("Inside Thread\n");
 	ThreadParams* params = (ThreadParams*)arg;
 	int m = params->m;
 	int n = params->n;
 	
-	pthread_exit(m+n);
+	// Casts the result to match the data type of result.
+	pthread_exit((void*)(intptr_t)(m+n));
 }
