2132f6032ac46cddfb64620a72973e344e73dae4
Marc Pham <112006435+48774861@users.noreply.github.com>
2025-02-28 01:57:03 +0000

    Update program2.c

--- program2.c
+++ program2.c
@@ -6,31 +6,47 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// Declarations for Thread Functions.
 void *thread1_function(void *arg);
 void *thread2_function(void *arg);
 
 int main() {
-	pthread_t thread1, thread2; //thread declaration
-	
-	void* file;
-	pthread_create(&thread1, NULL, thread1_function, NULL);
-	//thread is created
-	pthread_join(thread1, &file); // process waits for thread to finish.
+  pthread_t thread1, thread2; // Thread declarations.
 
+  // Stores the File Descriptor returned by thread1.
+  void* file;
+  
+  // Creates and waits for thread1 to open and return the file descriptor.
+  pthread_create(&thread1, NULL, thread1_function, NULL);
+  pthread_join(thread1, &file);
+  
+  // Creates and waits for thread2 to write to test.txt
   pthread_create(&thread2, NULL, thread2_function, file);
-  pthread_join(thread2, NULL); // process waits for thread to finish.
+  pthread_join(thread2, NULL);
 	
 }
+
 void *thread1_function(void *arg) {
-	// the work to be done by the thread is defined in this function
-  int fd = open("test.txt",O_WRONLY);
-	pthread_exit(fd);
+  // Opens "test.txt" and allow write-only and create access
+  int fd = open("test.txt",O_WRONLY|O_CREAT,0642);
+  
+  // Casts the file descriptor as void* so file will accept it.
+  pthread_exit((void*)(intptr_t)fd);
 }
+
 void *thread2_function(void *arg) {
-	// the work to be done by the thread is defined in this function
-  int fd = atoi(arg);
-  n = 12;
+  // Get the int file descriptor from void* 
+  int fd = (int)(intptr_t)arg;
+  
+  int n = 12;
   char buff[50] = "Hello its T2";
-  write(fd, buff, n);
+  
+  // Write the text above to the file and output something if it succeeds or fails.
+  if (write(fd, buff, n) == -1) {
+    perror("Failed to write");
+  } else {
+    printf("Successfully wrote to the file.\n");
+  }
+  
   pthread_exit(NULL);
 }
